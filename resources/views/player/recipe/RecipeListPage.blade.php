9305c994f95310401e29dc3037328a6eae065c4e
Paulius Skliaudys <paulius.skliaudys@ktu.edu>
2023-05-04 12:33:08 +0300

    added recipe reccommendation functionality

--- resources/views/player/recipe/RecipeListPage.blade.php
+++ resources/views/player/recipe/RecipeListPage.blade.php
@@ -10,10 +10,56 @@
         </div>
     @endif
     <h1 style="text-align: center; font-size: 50px;">Rekomendacijos</h1>
-    @if (false)
-    @else
+    <div class="row">
+        @foreach ($recommendedRecipes as $recipe)
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header">
+                        {{ $recipe->name }}
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Preperiation:</h5>
+                        {{ $recipe->preparation }}
+                    </div>
+                    <div class="card-body">
+                        Rating: {{ $recipe->rating }}
+                        Calories: {{ $recipe->calories }}
+                        Time: {{ $recipe->preparation_time }}
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Ingredients:</h5>
+                        <ul>
+                            @foreach ($recipe->products as $product)
+                                <li>{{ $product->name }} - {{ $product->pivot->quantity }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+
+                    <div class="card-body">
+                        <h5 class="card-title">Irankiai:</h5>
+                        <ul>
+                            @foreach ($recipe->tools as $tool)
+                                <li>{{ $tool->name }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+
+                    <div class="card-body">
+                        <h5 class="card-title">Virtive:</h5>
+                        <ul>
+                            @foreach ($recipe->kitchen_categories as $category)
+                                <li>{{ $category->name }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+
+    {{-- @else
         <h1 style="text-align: center; font-size: 50px;">Nera rekomendaciju</h1>
-    @endif
+    @endif --}}
     <h1 style="text-align: center; font-size: 50px;">Visi receptai</h1>
     <div class="row">
         @foreach ($recipes as $recipe)
